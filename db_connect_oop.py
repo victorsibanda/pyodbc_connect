36d4e4e149f25394f969fe59c93c22424c5a364d
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-12 15:56:55 +0000

    using classes and object

---
+++ db_connect_oop.py
@@ -0,0 +1,24 @@
+import pyodbc
+
+
+class MSDBConnection():
+
+    def __init__(self):
+        # Variables for connection
+        self.database = 'Northwind'
+        self.username = 'SA'
+        self.password = 'Passw0rd2018'
+        # Making the connection
+        self.conn = pyodbc.connect(
+            'DSN=MYMSSQL;UID=' + self.username + ';PWD=' + self.password + ';DATABASE=' + self.database)
+        # Making cursor
+        self.cursor = self.conn.cursor()
+
+    def sql_query(self, query):
+        return self.cursor.execute(query)
+
+
+nw_db_object = MSDBConnection()
+
+nw_db_object.sql_query('SELECT * FROM Products')
+print(nw_db_object)
